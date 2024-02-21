10a64bd7286a5aac97ab44c62597784182eadf1c
brunorafael8 <bruninho_lito@hotmail.com>
2024-02-20 21:04:40 -0300

    feat: create useFetchMatches hook on project

---
+++ src/hooks/index.ts
@@ -0,0 +1,1 @@
+export { useFetchMatches } from './useFetchMatches';
---
+++ src/hooks/useFetchMatches.ts
@@ -0,0 +1,38 @@
+import {useInfiniteQuery} from '@tanstack/react-query';
+import api from '../services/api';
+
+export type MatchProps = {
+  begin_at: string;
+  league_id: number;
+  status: 'canceled' | 'finished' | 'not_started' | 'postponed' | 'running';
+  league: {
+    name: string;
+    image_url: string | null;
+  };
+  serie_id: number;
+  serie: {
+    name: string;
+  };
+  opponents: {
+    opponent: {
+      id: number;
+      name: string;
+      image_url: string | null;
+    };
+  }[];
+};
+
+async function fetchMatches({pageSize = 10}) {
+  const res = await api.get(`/csgo/matches?page[size]=${pageSize}`);
+
+  return res.data;
+}
+
+export function useFetchMatches() {
+  return useInfiniteQuery({
+    queryKey: ['matches'],
+    queryFn: ({pageParam}) => fetchMatches({pageSize: pageParam}),
+    initialPageParam: 10,
+    getNextPageParam: lastPage => {},
+  });
+}
