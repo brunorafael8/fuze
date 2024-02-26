8f2b3fb8193fe8760881344a9dedbc7c7232f801
brunorafael8 <bruninho_lito@hotmail.com>
2024-02-26 09:59:04 -0300

    feat: add ios splash screen config

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,21 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import {
   NavigationContainer,
   useNavigationContainerRef,
 } from '@react-navigation/native';
-import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
-import {Navigator} from './navigator';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import SplashScreen from 'react-native-splash-screen';
 import '../global.css';
+import { Navigator } from './navigator';
 
 const queryClient = new QueryClient();
 
 const App = () => {
   const navigationRef = useNavigationContainerRef();
+
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
 
   return (
     <QueryClientProvider client={queryClient}>
